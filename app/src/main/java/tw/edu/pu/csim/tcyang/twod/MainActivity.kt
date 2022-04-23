4591a85a43582f944f1ae73acf4d1b21d26d3e55
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-23 11:44:20 +0800

    v.1版面設計及取代findViewById

--- app/src/main/java/tw/edu/pu/csim/tcyang/twod/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/twod/MainActivity.kt
@@ -2,10 +2,21 @@ package tw.edu.pu.csim.tcyang.twod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tw.edu.pu.csim.tcyang.twod.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var secondsLeft:Int = 1000  //倒數
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txv.text = secondsLeft.toString()
+        binding.btnStart.isEnabled = true
+        binding.btnStop.isEnabled = false
     }
 }
